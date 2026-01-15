c150552e07a962fe06ba56dc35c3fdad1848fe0f
ayman20-py <aycas2004@gmail.com>
2026-01-15 13:40:48 +0800

    Just another commit

---
+++ LinkedList_Version/Seat Lookup & Manifest/main_return.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "../passengerNode.cpp"
+
+using namespace std;
+
+int main() {
+
+    PassengerNode x = PassengerNode("1", "John Doe", 5, 3, "Economy", true);
+
+    return 0;
+}
